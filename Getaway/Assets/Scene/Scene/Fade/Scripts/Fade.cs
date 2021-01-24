e2c83a48fb2ed2b9ee0845f9a0c1e3926f35d5f7
Eto <akimagi26@gmail.com>
2021-01-24 21:37:05 +0900

    タイトル画面の編集中

---
+++ Getaway/Assets/Scene/Scene/Fade/Scripts/Fade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    // imageの参照
+    Image m_Image;
+
+    void Start() {
+        // Imageを取得
+        m_Image = GetComponent<Image>();
+    }
+
+    void Update() {
+    }
+
+    /// <summary>フェードイン</summary>
+    void FadeIn() {
+        m_Image.DOFade(0.0f, 1.0f);
+    }
+
+    /// <summary>フェードアウト</summary>
+    void FadeOut() {
+        m_Image.DOFade(1.0f, 1.0f);
+    }
+}
---
+++ Getaway/Assets/Scene/TitleScene/Script/TitleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    // Fadeオブジェクト
+    GameObject m_Fade;
+
+    void Start() {
+        // Fadeオブジェクトを取得
+        m_Fade = GameObject.FindGameObjectWithTag("Fade");
+    }
+
+    void Update() {   
+    }
+}
