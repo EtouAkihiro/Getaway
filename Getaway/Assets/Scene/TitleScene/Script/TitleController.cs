7019e5c9785f717f3d2c0ee15c73f69fa47ebb7a
Eto <akimagi26@gmail.com>
2021-01-25 09:12:15 +0900

    タイトルロゴのアニメーションを作成中

--- Getaway/Assets/Scene/TitleScene/Script/TitleController.cs
+++ Getaway/Assets/Scene/TitleScene/Script/TitleController.cs
@@ -9,12 +9,32 @@ public class TitleController : MonoBehaviour
 {
     // Fadeオブジェクト
     GameObject m_Fade;
+    // タイトルロゴ
+    GameObject m_TitleLogos;
 
     void Start() {
         // Fadeオブジェクトを取得
         m_Fade = GameObject.FindGameObjectWithTag("Fade");
+        // タイトルロオブジェクトを取得
+        m_TitleLogos = GameObject.Find("TitleLogos");
+
+        // フェードイン
+        FadeIn();
     }
 
     void Update() {   
+    }
+
+    /// <summary>フェードイン</summary>
+    void FadeIn() {
+        m_Fade.GetComponent<Image>().DOFade(0.0f, 1.0f);
+    }
+
+    /// <summary>フェードアウト(シーン遷移付き)</summary>
+    void FadeOut() {
+        m_Fade.GetComponent<Image>().DOFade(1.0f, 1.0f).OnComplete(() => {
+            // シーン遷移
+            // SceneManager.LoadScene("");
+        });
     }
 }
