9a6a706d64a7413fbd8d7fcccf0e8e1fb8bec402
Eto_Akihiro <akimagi26@gmail.com>
2021-02-03 11:34:36 +0900

    TNOPシーンのカメラのアニメーションを追加・キャンバスのアニメーションを追加

--- Getaway/Assets/Scene/TNOPScene/Scripts/TNOPController.cs
+++ Getaway/Assets/Scene/TNOPScene/Scripts/TNOPController.cs
@@ -1,19 +1,178 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class TNOPController : MonoBehaviour
 {
+    /// <summary>一人でプレイする画面の初期設定のプレイボタン</summary>
+    public GameObject m_SoloPlayButton;
+    /// <summary>複数人でプレイする画面の初期設定のプレイボタン</summary>
+    public GameObject m_PluralPlayButton;
+
     /// <summary>フェードの参照</summary>
     Fade m_Fade;
 
-    void Start() {
-        // フェードオブジェクトの取得
-        m_Fade = GameObject.FindGameObjectWithTag("Fade").GetComponent<Fade>();
-        // フェードイン
-        m_Fade.FadeIn();
+    enum State
+    {
+        Select,
+        Solo,
+        plural
     }
 
-    void Update() {    
+    /// <summary>一人でプレイする画面のオブジェクト</summary>
+    RectTransform m_SoloSelectObjects;
+    /// <summary>複数人でプレイする画面のオブジェクト</summary>
+    RectTransform m_PluralPlaySelectObjects;
+
+    // 状態
+    State m_State;
+    // メインカメラアニメーター
+    Animator m_MainCameraAnimator;
+    // キャンバスアニメーター
+    Animator m_CanvasAnimator;
+
+    // 一人でプレイする画面に移動するアニメーション
+    int s_SoloSelectHash = Animator.StringToHash("SoloSelectFrag");
+    // 複数人でプレイする画面に移動するアニメーション
+    int s_PluralPlaySelectHash = Animator.StringToHash("PluralPlaySelectFrag");
+
+    // 一人でプレイする画面を表示するアニメーション
+    int s_SoloSelectDispPlayHash = Animator.StringToHash("soloSelectFrag");
+    // 複数でプレイする画面を表示するアニメーション
+    int s_PluralPlaySelectDispPlayHash = Animator.StringToHash("PluralPlaySelectFrag");
+
+    void Start() {
+        // フェードオブジェクトの取得
+        // m_Fade = GameObject.FindGameObjectWithTag("Fade").GetComponent<Fade>();
+        // メインカメラのアニメーターの取得
+        m_MainCameraAnimator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+        // キャンバスのアニメーターの取得
+        m_CanvasAnimator = GameObject.Find("TNOPCanvas").GetComponent<Animator>();
+        // 一人でプレイする画面のオブジェクトを取得
+        m_SoloSelectObjects = GameObject.Find("soloSelectObjects").GetComponent<RectTransform>();
+        // 複数人でプレイする画面のオブジェクトを取得
+        m_PluralPlaySelectObjects = GameObject.Find("PluralPlaySelectObjects").GetComponent<RectTransform>();
+
+        // フェードイン
+        // m_Fade.FadeIn();
+        // 初期状態をアイドル
+        m_State = State.Select;
+    }
+
+    void Update() {
+        // 状態の更新
+        Update_state();
+    }
+
+    /// <summary>状態の更新</summary>
+    void Update_state() {
+        // 状態ごとに更新
+        switch (m_State)
+        {
+            case State.Select: Idel(); break;
+            case State.Solo: Solo(); break;
+            case State.plural: Plural(); break;
+        }
+    }
+
+    /// <summary>アイドル状態</summary>
+    void Idel() {
+        // 左右の入力値
+        float H = Input.GetAxis("Horizontal");
+
+        // 閾値
+        const float Threshold = 0.9f;
+
+        // 入力値がゼロ以外だった場合
+        if (H != 0) {
+            // 左に入力された場合、状態を一人でプレイする状態にする
+            if (H < -Threshold) m_State = State.Solo;
+            // 右に入力された場合、状態を複数人でプレイする状態にする
+            if (H > Threshold) m_State = State.plural;
+        }
+
+        // 状態がアイドルから変わった場合
+        if (m_State != State.Select) {
+            // 状態が、一人でプレイする状態だった場合
+            if(m_State == State.Solo) {
+                // 一人でプレイする画面を表示
+                m_CanvasAnimator.SetBool(s_SoloSelectDispPlayHash, true);
+                // 一人でプレイする状態のアニメーションを再生
+                m_MainCameraAnimator.SetBool(s_SoloSelectHash, true);
+            }
+            // 状態が、複数人でプレイする状態だった場合
+            else if (m_State == State.plural) {
+                // 複数人でプレイする画面を表示
+                m_CanvasAnimator.SetBool(s_PluralPlaySelectDispPlayHash, true);
+                // 複数人でプレイする状態のアニメーションを再生
+                m_MainCameraAnimator.SetBool(s_PluralPlaySelectHash, true);
+            }
+        }
+    }
+
+    /// <summary>一人プレイ</summary>
+    void Solo() {
+        // 選択されているオブジェクトがなかった場合
+        if(EventSystem.current.currentSelectedGameObject == null && m_SoloSelectObjects.localPosition.x == 0) {
+            // >一人でプレイする画面のプレイボタンをセット
+            EventSystem.current.SetSelectedGameObject(m_SoloPlayButton);
+        }
+    }
+
+    /// <summary>複数人でプレイ</summary>
+    void Plural() {
+        // 選択されているオブジェクトがなかった場合
+        if (EventSystem.current.currentSelectedGameObject == null && m_PluralPlaySelectObjects.localPosition.x == 0) {
+            // >複数人でプレイする画面のプレイボタンをセット
+            EventSystem.current.SetSelectedGameObject(m_PluralPlayButton);
+        }
+    }
+
+    /// <summary>ソロプレイでゲームを開始する</summary>
+    public void OnClick_SoloPlay() {
+    }
+
+    /// <summary>複数人でゲームを開始する</summary>
+    public void OnClick_PluralPlay() {
+    }
+
+    /// <summary>一人でプレイするから選択画面に戻る</summary>
+    public void OnClick_SoloBack() {
+        // 現在の状態が、一人でプレイする状態だった場合
+        if(m_State == State.Solo) {
+            // 選択画面へ移動するアニメーションを再生
+            m_MainCameraAnimator.SetBool(s_SoloSelectHash, false);
+            // 選択画面を表示
+            m_CanvasAnimator.SetBool(s_SoloSelectDispPlayHash, false);
+            // 状態を選択状態に遷移
+            m_State = State.Select;
+            // 現在の選択されているオブジェクトがある場合
+            if(EventSystem.current.currentSelectedGameObject != null) {
+                // 選択されているオブジェクトを無しにする
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
+    }
+
+
+    /// <summary>複数人でプレイするから選択画面へ戻る</summary>
+    public void OnClick_PluralBack() {
+        // 現在の状態が、複数人でプレイする状態だった場合
+        if(m_State == State.plural) {
+            // 選択画面へ移動するアニメーションを再生
+            m_MainCameraAnimator.SetBool(s_PluralPlaySelectHash, false);
+            // 選択画面を表示
+            m_CanvasAnimator.SetBool(s_PluralPlaySelectDispPlayHash, false);
+            // 状態を選択状態に遷移
+            m_State = State.Select;
+            // 現在の選択されているオブジェクトがある場合
+            if (EventSystem.current.currentSelectedGameObject != null) {
+                // 選択されているオブジェクトを無しにする
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
     }
 }
