bebe027787da238fe827a5dc9c80b0583161ea45
Eto_Akihiro <akimagi26@gmail.com>
2021-02-20 20:57:52 +0900

    一体の敵の徘徊ルートをランダム化を実装(仮)

--- Getaway/Assets/Enemy/Scripts/Thc6/Thc6.cs
+++ Getaway/Assets/Enemy/Scripts/Thc6/Thc6.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class Thc6 : MonoBehaviour
 {
     /// <summary>徘徊ルート</summary>
-    public Transform[] m_PatrolPoints;
+    GameObject[] m_PatrolPoints;
 
     /// <summary>キャラクターコントローラー</summary>
     CharacterController m_CharacterController;
@@ -15,13 +15,17 @@ public class Thc6 : MonoBehaviour
     /// <summary>アニメーター</summary>
     Animator m_Animator;
 
-    /// <summary>現在の徘徊ルートのカウンター</summary>
+    /// <summary>現在の徘徊ルート</summary>
     int m_CurrentPatrolPointIndex = -1;
+    /// <summary>前回の徘徊ルートの番号を保存する変数</summary>
+    Queue m_LastTimePatrolPointIndex = new Queue(15);
 
     /// <summary>重力加速度</summary>
     float m_Gravity = -9.81f;
 
     void Start() {
+        // 徘徊ルートを取得
+        m_PatrolPoints = GameObject.FindGameObjectsWithTag("thc6_PatrolPoint");
         // キャラクターコントローラーの取得
         m_CharacterController = GetComponent<CharacterController>();
         // ナビメッシュエージェントの取得
@@ -44,15 +48,35 @@ public class Thc6 : MonoBehaviour
 
     /// <summary>次に向かう場所を決定</summary>
     void SetNewPatrolPointToDestination() {
-        // 徘徊ルートのカウンターアップ
+
+        // 徘徊ルートの番号を保存
+        m_LastTimePatrolPointIndex.Enqueue(m_CurrentPatrolPointIndex);
+        // 徘徊ルートのランダム
         m_CurrentPatrolPointIndex = Random.Range(0, m_PatrolPoints.Length);
+        // 今まで保存していた徘徊ルートの番号と比較し、同じ番号がある場合
+        while (m_LastTimePatrolPointIndex.Contains(m_CurrentPatrolPointIndex)) {
+            // 再ランダム
+            m_CurrentPatrolPointIndex = Random.Range(0, m_PatrolPoints.Length);
+            // 今まで保存していた徘徊ルートの番号と比較し、同じ番号がない場合
+            if (!m_LastTimePatrolPointIndex.Contains(m_CurrentPatrolPointIndex)) {
+                // 無限ループから抜ける
+                break;
+            }
+        }
+
         // 現在の徘徊ルートが現在設定されている徘徊ルートの番号以上だった場合
-        if(m_CurrentPatrolPointIndex >= m_PatrolPoints.Length) {
+        if (m_CurrentPatrolPointIndex >= m_PatrolPoints.Length) {
             // 現在の徘徊ルートのリセット
             m_CurrentPatrolPointIndex = 0;
         }
         // 現在の徘徊ルートを反映
-        m_NavMeshAgent.destination = m_PatrolPoints[m_CurrentPatrolPointIndex].position;
+        m_NavMeshAgent.destination = m_PatrolPoints[m_CurrentPatrolPointIndex].transform.position;
+
+        // 保存するキューの要素数が15以上になった場合
+        if(m_LastTimePatrolPointIndex.Count >= 15) {
+            // 古い要素を削除
+            m_LastTimePatrolPointIndex.Dequeue();
+        }
     }
 
     /// <summary>到着したか？</summary>
