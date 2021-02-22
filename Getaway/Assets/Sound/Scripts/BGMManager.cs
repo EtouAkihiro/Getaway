52262dfa362f4ba7ad2abfb2564df49bc5778951
Eto_Akihiro <akimagi26@gmail.com>
2021-02-22 13:19:48 +0900

    BGMマネージャーのスクリプトを作成

---
+++ Getaway/Assets/Sound/Scripts/BGMManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class BGMManager : MonoBehaviour
+{
+    [SerializeField, Header("タイトルBGM")]
+    private AudioClip m_TitleBGM;
+
+    // オーディオソース
+    AudioSource m_AudioSource;
+
+    void Awake() {
+        // 全シーンにまたがって存在する（シーン切り替えで破壊されない）ようにする
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start() {
+        // オーディオソースを参照
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+    }
+
+    /// <summary>BGMの再生</summary>
+    /// <param name="audioClip">サウンドクリップ</param>
+    void PlayBGM(AudioClip audioClip) {
+        // 現在のBGMと同じBGMじゃあない場合
+        // または、クリップの中に何もなかった場合
+        if(m_AudioSource.clip != audioClip ||
+           m_AudioSource.clip == null) {
+            // 入れ替えて、再生
+            m_AudioSource.clip = audioClip;
+            m_AudioSource.Play();
+        }
+    }
+
+    /// <summary>BGMの停止</summary>
+    void StopBGM() {
+        // もし、BGMの再生されていた場合
+        if (m_AudioSource.isPlaying) {
+            // BGMの停止
+            m_AudioSource.Stop();
+            // サウンドクリップの中身を空にする
+            m_AudioSource.clip = null;
+        }
+    }
+
+    /// <summary>BGMマネージャーが2個以上存在する場合、新しく生成されたBGMマネージャーを削除</summary>
+    void BGMManagerTwoOrmore() {
+        // 現在のシーンにあるBGMマネージャーを取得
+        GameObject[] BGMManagers = GameObject.FindGameObjectsWithTag("BGMManager");
+
+        // BGMマネージャーが2個以上存在する場合
+        if(BGMManagers.Length >= 2) {
+            // 新しく生成されたBGMマネージャーを削除
+            Destroy(BGMManagers[BGMManagers.Length - 1]);
+        }
+    }
+}
