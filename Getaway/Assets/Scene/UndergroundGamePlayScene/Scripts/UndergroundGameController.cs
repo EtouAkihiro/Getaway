f533d4b1ae3a3863ce3fb96cdb4fc2d10c82904c
Eto_Akihiro0206 <akimagi26@gmail.com>
2021-04-12 16:17:53 +0900

    地下のステージシーンにコントローラーを追加

---
+++ Getaway/Assets/Scene/UndergroundGamePlayScene/Scripts/UndergroundGameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>地下のステージのシーンの管理</summary>
+public class UndergroundGameController : MonoBehaviour
+{
+    void Start()
+    {
+        // フェイドインする。
+        Fade.Instance.FadeIn();
+    }
+}
